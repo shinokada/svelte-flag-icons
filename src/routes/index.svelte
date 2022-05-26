57a380890fd8f63a963bb1e336a2274320d44f3d
Shinichi Okada <okada.shin@gmail.com>
2022-05-26 14:03:10 +0900

    docs: update README

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,16 +3,29 @@
 </script>
 
 <div class="flags">
-	<Us size="200" />
-	<Ca size="200" />
-	<Fr size="200" />
-	<De size="200" />
-	<Dk size="200" />
-	<Jp size="200" />
-	<No size="200" />
-	<Ch size="200" />
-	<Cz size="200" />
+	<Us size="100" />
+	<Ca size="100" />
+	<Fr size="100" />
+	<De size="100" />
+	<Dk size="100" />
+	<Jp size="100" />
+	<No size="100" />
+	<Ch size="100" />
+	<Cz size="100" />
 </div>
+
+<div class="flags">
+	<Us size="100" class="rounded-full bg-white ring-2 rring-gray-300 m-4" />
+	<No size="100" class="rounded-full bg-white ring-2 ring-gray-300 m-4" />
+	Tailwind CSS + Size
+	<Ca class="rounded-full bg-white ring-2 ring-gray-300 m-4" size="100" />
+	Tailwind CSS + size null
+	<Fr class="rounded-full bg-white ring-2 ring-gray-300 m-4" size="100" />
+</div>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" />
+</svelte:head>
 
 <style>
 	.flags {
