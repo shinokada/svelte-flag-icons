a28302fffa2c0ba59c221ba485151c801fca9415
Shinichi Okada <okada.shin@gmail.com>
2023-08-03 17:29:28 +0200

    feat: use two files

--- src/routes/icons/+page.svelte
+++ src/routes/icons/+page.svelte
@@ -1,46 +1,38 @@
 <script>
   import Label from 'flowbite-svelte/Label.svelte';
   import Range from 'flowbite-svelte/Range.svelte';
+  import Tabs from 'flowbite-svelte/Tabs.svelte';
+  import TabItem from 'flowbite-svelte/TabItem.svelte';
   import TableSearch from 'flowbite-svelte/TableSearch.svelte';
+  import Icon from '$lib/Icon.svelte';
+  import icons from '$lib/icons.js';
+  import { random_tailwind_color, random_hex_color_code} from '../utils.js'
 
-  import * as Icons from '$lib';
-
-  const random_tailwind_color = () => {
-    const colors = ['red', 'yellow', 'green', 'blue', 'indigo', 'purple', 'pink'];
-    const shades = ['300', '400', '500'];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    const randomShade = shades[Math.floor(Math.random() * shades.length)];
-    return `text-${randomColor}-${randomShade} dark:text-${randomColor}-${randomShade} shrink-0 h-8 w-8`;
-  };
-  const random_hex_color_code = () => {
-    let n = (Math.random() * 0xfffff * 1000000).toString(16);
-    return '#' + n.slice(0, 6);
-  };
-  const contentClass = 'rounded-lg dark:bg-neutral-950 mt-4';
+  const contentClass = 'rounded-lg dark:bg-cyan-950 mt-4';
   let searchTerm = '';
 
-  $: filteredEntries = Object.entries(Icons).filter(([name, component]) => {
+  $: filteredIconNames = Object.keys(icons).filter(name => {
     return name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
   });
-  let size="24";
+  let size="30"
 </script>
-<h1>Svelte Flag Icons: Icons</h1>
+<h1>Svelte Flag Icons</h1>
 <TableSearch
   placeholder="Search by icon name"
   hoverable={true}
   bind:inputValue={searchTerm}
   divClass='relative overflow-x-auto'
 >
-<div class="xl:w-1/3 lg:w-2/5 md:w-1/2 sm:w-3/4 w-full p-4">
+<div class="xl:w-1/4 lg:w-1/3 md:w-1/2 sm:w-3/4 w-full p-4">
   <Label class="text-lg py-4 ">Icon size: {size}</Label>
-  <Range id="range1" min="18" max="50" bind:value={size} />
+  <Range id="range1" min="20" max="50" bind:value={size} />
 </div>
-<div class="grid 2xl:grid-cols-7 xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-8 px-4 dark:text-white">
-  {#each filteredEntries as [name, component]}
+  <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8 px-4 dark:text-white">
+    {#each filteredIconNames as iconName, i}
     <div class="flex gap-4 items-center text-lg">
-      <svelte:component this={component} class="shrink-0" bind:size={size} />
-      {name}
+      <Icon name={iconName} bind:width={size} bind:height={size} class="shrink-0"/>
+      {iconName}
     </div>
-  {/each}
-</div>
+    {/each}
+  </div>
 </TableSearch>
