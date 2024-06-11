deb929356bd9d8b6ceaec7f838b2e70e05f30013
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-06-11 18:05:47 +0200

    tests: updates

--- src/routes/flags/+page.ts
+++ src/routes/flags/+page.ts
@@ -2,10 +2,11 @@ import type { MetaProps } from 'runes-meta-tags';
 
 const title = 'Flags - Svelte Flag Icons'
 const description = 'Easily Find & Copy SVG Icon name for Your Project'
-const imgUrl = 'https://open-graph-vercel.vercel.app/api/svelte-flags'
-const ogUrl = 'http://localhost:4173/flags'
+const imgUrl = 'https://open-graph-vercel.vercel.app/api/svelte-flag-icons'
+
 
 export const load = ({ url }) => {
+  const ogUrl = url.href
   const pageMetaTags: MetaProps = {
     title,
     description,
--- tests/flags.test.ts
+++ tests/flags.test.ts
@@ -1,11 +1,11 @@
 import { expect, test } from '@playwright/test';
-const title = 'Icons - Svelte Flag Icons'
+const title = 'Flags - Svelte Flag Icons'
 const description = 'Easily Find & Copy SVG Icon name for Your Project'
 const imgUrl = 'https://open-graph-vercel.vercel.app/api/svelte-flag-icons'
 
 test.beforeEach(async ({ page }) => {
   console.log(`Running ${test.info().title}`);
-  await page.goto('/icons');
+  await page.goto('/flags');
 });
 
 test('Icons page has expected h1, meta title', async ({ page }) => {
@@ -21,13 +21,14 @@ test('Icons page has expected meta description', async ({ page }) => {
   await expect(metaDescription).toHaveAttribute('content', description);
 });
 
-test('Icons page has expected meta og', async ({ page }) => {
+test('Icons page has expected meta og', async ({ page, baseURL }) => {
   const metaOgTitle = page.locator('meta[property="og:title"]');
   await expect(metaOgTitle).toHaveAttribute('content', title);
   const metaOgDescription = page.locator('meta[property="og:description"]');
   await expect(metaOgDescription).toHaveAttribute('content', description);
   const metaOgUrl = page.locator('meta[property="og:url"]');
-  await expect(metaOgUrl).toHaveAttribute('content', 'http://localhost:4173/icons');
+  const url = baseURL + '/flags';
+  await expect(metaOgUrl).toHaveAttribute('content', url);
   const metaOgImage = page.locator('meta[property="og:image"]');
   await expect(metaOgImage).toHaveAttribute(
     'content',
