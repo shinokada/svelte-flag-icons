437c1e27ffa41395917cae6ab3edbc31f96cbfd3
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-11-02 11:05:16 +0100

    fix: lint fixes

--- src/routes/guide/svelte-5/getting-started/+page.ts
+++ src/routes/guide/svelte-5/getting-started/+page.ts
@@ -4,7 +4,7 @@ const title = 'Getting Started - Svelte Flag Icons v2';
 const description = 'How to get started with Svelte Flag Icons v2';
 const imgUrl = 'https://open-graph-vercel.vercel.app/api/svelte-flag-icons';
 
-export const load = ({ url }) => {
+export const load = () => {
   const pageMetaTags: MetaProps = {
     title,
     description,
