8578208795503ee189ecd77d4dbd5f951f031dc2
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-12-06 14:33:15 +0100

    fix: use svelte-shields

--- src/routes/guide/svelte-4/getting-started/+page.svelte
+++ src/routes/guide/svelte-4/getting-started/+page.svelte
@@ -1,5 +1,34 @@
 <script lang="ts">
   import { HighlightCompo, Code, H2 } from 'runes-webkit';
+  import { GitHubSponsor, type GitHubSponsorPropsType,  NpmVersion, type NpmVersionPropsType, License, type LicensePropsType, NpmDownload, type NpmDownloadPropsType } from 'svelte-shields';
+
+  const sponsor: GitHubSponsorPropsType = {
+    user: 'shinokada',
+    logo:'github',
+    link: ['https://github.com/sponsors/shinokada', 'https://github.com/sponsors/shinokada']
+  };
+  const npmversion: NpmVersionPropsType = {
+    packageName: 'svelte-flag-icons',
+    tag:'v1-latest',
+    logo:'svelte',
+    link: [
+      'https://www.npmjs.com/package/svelte-flag-icons',
+      'https://www.npmjs.com/package/svelte-flag-icons?activeTab=versions'
+    ]
+  };
+  const license: LicensePropsType = {
+    source: 'github',
+    github_user: 'shinokada',
+    github_repo: 'svelte-flag-icons',
+    link: [
+      'https://github.com/shinokada/svelte-flag-icons/blob/master/LICENSE',
+      'https://github.com/shinokada/svelte-flag-icons/blob/master/LICENSE'
+    ]
+  };
+  const downloads: NpmDownloadPropsType = {
+    packageName: 'svelte-flag-icons',
+    link: ['https://www.npmjs.com/package/svelte-flag-icons']
+  };
 
   const modules = import.meta.glob('./md/*.md', {
     query: '?raw',
@@ -11,36 +40,10 @@
 <h1>Getting Started - Svelte Flag Icons v1</h1>
 
 <div class="my-8 flex gap-2">
-  <a href="https://github.com/sponsors/shinokada" target="_blank" aria-label="Sponsor shinokada"
-    ><img
-      src="https://img.shields.io/static/v1?label=Sponsor&message=%E2%9D%A4&logo=GitHub&color=%23fe8e86"
-      alt="sponsor"
-    /></a
-  >
-  <a
-    href="https://www.npmjs.com/package/svelte-flag-icons"
-    rel="nofollow"
-    target="_blank"
-    aria-label="npm svelte-flag-icons"
-    ><img src="https://img.shields.io/npm/v/svelte-flag-icons" alt="npm" /></a
-  >
-  <a
-    href="https://opensource.org/licenses/MIT"
-    rel="nofollow"
-    target="_blank"
-    aria-label="MIT License"
-    ><img
-      src="https://img.shields.io/github/license/shinokada/svelte-flag-icons"
-      alt="License"
-    /></a
-  >
-  <a
-    href="https://www.npmjs.com/package/svelte-flag-icons"
-    rel="nofollow"
-    target="_blank"
-    aria-label="npm svelte-flag-icons"
-    ><img src="https://img.shields.io/npm/dw/svelte-flag-icons.svg" alt="npm" /></a
-  >
+  <GitHubSponsor {...sponsor} />
+  <NpmVersion {...npmversion} />
+  <License {...license} />
+  <NpmDownload {...downloads} />
 </div>
 
 <H2>Requirements</H2>
