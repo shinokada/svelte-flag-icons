70dc468d571b9090d37d0c83a4f50bf0477b7d76
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-03-10 15:24:17 +0100

    fix: update svelte and tailwindcss 4

---
+++ src/routes/testdir/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { Ad } from '$lib';
+  import MyIcon from './MyIcon.svelte'
+</script>
+
+<div class="p-16">
+
+  <h1>Test</h1>
+  <MyIcon Icon={Ad} />
+</div>
---
+++ src/routes/testdir/MyIcon.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { Icon as FlagIcon } from '$lib';
+  import { type Component } from 'svelte';
+  const config: { size: string, ariaLabel: string, class: string } = {
+    size: "100",
+    ariaLabel: "my custom icon",
+    class: 'mx-4'
+  };
+  interface Props {
+    Icon: Component
+  }
+
+  let { Icon }: Props = $props();
+</script>
+<FlagIcon {...config} {Icon} />
